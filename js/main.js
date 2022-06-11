5a6eb1063a5a372bd36c96be3d650bd1f2583bc8
fatihyazicioglu <f.yzcgl1@gmail.com>
2022-06-11 23:18:40 +0200

    updated

--- js/main.js
+++ js/main.js
@@ -9,19 +9,35 @@ let currentStep = formSteps.findIndex(step =>{
 
 if (currentStep <0){
     currentStep = 0
-    formSteps[currentStep].classList.add ("active")
+    showCurrentStep()
+    // formSteps[currentStep].classList.add ("active")
 }
 
 
-// function for the Next-Previous button
+// function for the Next-Previous button with reportValidity()
 multiStepForm.addEventListener("click", e =>{
+    let incrementor
     if(e.target.matches("[data-next]")){
-        currentStep +=1
+        incrementor = 1
     }
     else if(e.target.matches("[data-previous]")){
-        currentStep -=1
+        incrementor = -1
     }
+    if(incrementor ==null) return
+const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+const allValid = inputs.every(input=>input.reportValidity())
+if(allValid){
+    currentStep +=incrementor
+}
+    showCurrentStep()
 })
 
 
-// hide and show the current step
+// hide and show the current step by toggleing the class "active"
+function showCurrentStep(){
+    formSteps.forEach((step,index) =>{
+        step.classList.toggle ("active", index===currentStep)
+    })
+}
+
+// i want to make sure that the inputs valid
